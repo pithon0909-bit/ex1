e1083a398d5ceffd69cc82771eba098282ffbf07
pithon0909-bit <pithon0909@gmail.com>
2025-11-20 16:39:51 +0100

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,10 +1,1 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder 编辑器
 
-这是一个临时脚本文件。
-"""
-for n in range(10, 1314):  # 1314 so that 1313 is included
-    if n % 13 == 0 and n % 5 == 0:
-        print(n)
-        
